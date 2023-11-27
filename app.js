51d31f54ff310181303769c0c0cc5df2e8d3abab
Miles Wilson <wilson.mil@icloud.com>
2023-11-27 16:28:31 -0500

    Initialize backend user code

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
+import dotenv from 'dotenv'
+
 import cors from "cors";
 
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
 
 
-const app = express()
-app.use(cors());
+
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app)
 Lab5(app);
 Hello(app);
 

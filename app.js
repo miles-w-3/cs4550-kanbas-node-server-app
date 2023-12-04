9967683d354d0f25de152ee3bc23f674ffca9dd0
Miles Wilson <wilson.mil@icloud.com>
2023-12-04 18:03:30 -0500

    Fix session options

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(
   session(sessionOptions)
 );
